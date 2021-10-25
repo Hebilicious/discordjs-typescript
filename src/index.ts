c5183e06454a62757ff67249fab267970758a043
Hebilicious <xsh4k3@gmail.com>
2021-10-25 22:58:11 +0100

    feat: Add a few easter eggs commands

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,20 @@
-import { Client, Intents, Message } from 'discord.js';
+import { Client, Intents, Message } from "discord.js"
 const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
-});
+})
 
-client.once('ready', () => {
-  console.log(`Logged in as ${client.user?.tag}!`);
-});
+client.once("ready", () => {
+  console.log(`Logged in as ${client.user?.tag}!`)
+})
 
-client.on('message', (msg: Message) => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong 🏓');
-  } else if (msg.content === 'hello') {
-    msg.reply('Choo choo! 🚅');
-  }
-});
+client.on("message", (msg: Message) =>
+  msg.reply(
+    {
+      train: "Choo choo! 🚅",
+      ping: "Pong 🏓!",
+      jump: "How high!"
+    }[msg.content]
+  )
+)
 
-client.login();
+client.login()
